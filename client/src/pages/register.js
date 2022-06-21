23b5918cb6e3d3a41cba0cb2700a9fa47de65866
CWilliamL <williamchu1996@gmail.com>
2022-06-21 16:19:43 +0800

    finish 43:00

---
+++ client/src/pages/register.js
@@ -0,0 +1,44 @@
+import { useContext, useState } from "react";
+import { authContext} from " ../context/authContext";
+import { useForm } from "../utility/hooks";
+import { useMutation} from "@apollo/react-hooks";
+
+import { gql} from 'graphql-tag';
+import { useNavigate } from "react-router-dom";
+
+const REGISTER_USER = gql`
+    mutation Mutation(
+        $registerInput: RegisterInput
+    ){
+        registerUser(
+            registerInput: $registerInput
+        ) {
+            email
+            username
+            token
+        }
+    }
+`
+
+function Register(props) {
+    const context = useContext(authContext);
+    let navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
+    const { onChange, onSubmit, values } = useForm(registerUserCallback, {
+        username:'',
+        email:'',
+        password:'',
+        confirmPassword: ''
+    })
+
+    const [ registerUser, {loading}] = useMutation(REGISTER_USER, {
+        update(proxy, { data:{registerUser:userData}}) {
+              context.login(userData); 
+              navigate('/');
+        },
+        onError({graphQLErrors}) {
+            setErrors(graphQLErrors);
+        }
+        variables: { registerInput:values}
+    })
+}
